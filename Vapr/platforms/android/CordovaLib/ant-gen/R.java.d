dfcc7740695fa5af7e751272e4061393d927966c
Ronak Raithatha <ronaksraithatha@gmail.com>
2014-08-23 18:27:15 -0400

    First Build

---
+++ Vapr/platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/ronak/Development/Hackathon-2014/davy-jones-acorneers-repo/Vapr/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
--- Vapr/platforms/android/assets/www/js/controllers.js
+++ Vapr/platforms/android/assets/www/js/controllers.js
@@ -11,6 +11,18 @@ angular.module('starter.controllers', [])
     $scope.modal = modal;
   });
 
+    $ionicModal.fromTemplateUrl('templates/crowdsource.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.crowdsourceModal = modal;
+    });
+
+    $ionicModal.fromTemplateUrl('templates/share.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.shareModal = modal;
+    });
+
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
     $scope.modal.hide();
@@ -20,6 +32,24 @@ angular.module('starter.controllers', [])
   $scope.login = function() {
     $scope.modal.show();
   };
+
+
+    $scope.closeCrowdsource = function() {
+        $scope.crowdsourceModal.hide();
+    };
+
+    $scope.crowdsource = function() {
+        $scope.crowdsourceModal.show();
+    };
+
+
+    $scope.closeShare = function() {
+        $scope.shareModal.hide();
+    };
+
+    $scope.share = function() {
+        $scope.shareModal.show();
+    };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
@@ -44,5 +74,50 @@ angular.module('starter.controllers', [])
   ];
 })
 
+.controller('HomeCtrl', function($scope){
+  $scope.featuredGame = function(){
+    //TODO: Add code to get most rated game
+    alert('Here you will see the highest rated(featured)game')
+  }
+})
+
+.controller('SubmitCtrl', function($scope){
+  
+  
+  $scope.gameData = {};
+  $scope.doSubmit = function(){
+    //get Maxid and make it your new game id
+    //add comment and max comment # to new list
+    //add that to unserialized gamesPost json
+    
+    var data = angular.fromJson(window.localStorage['gamesPost']);
+    var json =angular.toJson($scope.gameData);
+    console.log($scope.gameData);
+    console.log(data);
+  }
+  $scope.tagActive = function(tag){
+    tag.isActive = !tag.isActive; 
+    $scope.gameData.isActive += '|' + tag.id;
+  }
+  
+  $scope.tags = [
+       { title: 'Action', id: 1 },
+       { title: 'Adventure', id: 2 },
+       { title: 'FPS', id: 3 },
+       { title: 'MMO', id: 4 },
+       { title: 'Role Playing', id: 5 },
+       { title: 'Sports', id: 6 },
+       ];
+})
+
+.controller('ViewAllCtrl', function($scope){
+  var data = angular.fromJson(window.localStorage['gamesPost']);
+  $scope.games=data['game'];
+  console.log(data['game']);
+  
+  
+})
+
 .controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+
+    });
